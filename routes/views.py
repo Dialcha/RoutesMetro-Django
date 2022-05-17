a9a06858e0696cde43119c0e622d10f63c8ed913
Diego <diego.chavarriap@gmail.com>
2022-05-17 10:34:08 -0500

    Lines and Stations works (CRUD)

---
+++ routes/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Line, Station, Stop
+from .forms import  StopForm, LineForm, StationForm
+# Add your imports below:
+from django.views.generic import TemplateView, ListView
+
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
+class HomeView(TemplateView):
+  template_name = "routes/home.html"
+
+  def get_context_data(self):
+    context = super().get_context_data()
+    context["lines"] = Line.objects.all()
+    context["stations"] = Station.objects.all()
+    context["stops"] = Stop.objects.all()
+    return context
+
+# Create your views here.
+class LinesView(ListView):
+  model = Line
+  template_name = "routes/lines.html"
+
+class CreateLineView(CreateView):
+  model = Line
+  form_class = LineForm
+  template_name = "routes/add_line.html"
+
+class UpdateLineView(UpdateView):
+  model = Line
+  form_class = LineForm
+  template_name = "routes/update_line.html"
+
+class DeleteLineView(DeleteView):
+  model = Line
+  template_name = "routes/delete_line.html"
+  success_url = "/lines/"
+
+class StationsView(ListView):
+  model = Station
+  template_name = "routes/stations.html"
+
+class CreateStationView(CreateView):
+  model = Station
+  template_name = "routes/add_station.html"
+  form_class = StationForm
+
+class UpdateStationView(UpdateView):
+  model = Station
+  template_name = "routes/update_station.html"
+  form_class = StationForm
+
+class DeleteStationView(DeleteView):
+  model = Station
+  template_name = "routes/delete_station.html"
+  success_url = "/stations/"
+
